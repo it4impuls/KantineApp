f2769d5665ea809e74a0b517cb394bc5fde05bfa
marilynias <marilynia57+github@gmail.com>
2025-07-31 09:35:19 +0200

    correct ln

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
-set -x
+set -x      # echo output
 dirname=${PWD##*/}
 target_dir=$HOME/$dirname
 
+rm -r "$target_dir"
 mkdir "$target_dir"
-ln -s "$PWD/*" "$target_dir/*"
-ln -s $HOME/.env $target_dir/
+ln -sr "$PWD/"* "$target_dir"
+# ln -s $HOME/.env $target_dir/
 cd "$target_dir"
 ls -a
 
 # Docker
-docker compose up -d --build
+# docker compose up -d --build
