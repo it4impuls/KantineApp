de2dc1072d47bbc958612c97db8554c8f16a5266
marilynias <marilynia57+github@gmail.com>
2025-07-31 09:36:33 +0200

    dont rem old dir

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -x      # echo output
 dirname=${PWD##*/}
 target_dir=$HOME/$dirname
 
-rm -r "$target_dir"
+# rm -r "$target_dir"
 mkdir "$target_dir"
 ln -sr "$PWD/"* "$target_dir"
 ln -s $HOME/.env $target_dir/
