471fb4a3f388d525bf4a361df23ef997899482b5
marilynias <marilynia57+github@gmail.com>
2025-07-31 10:34:20 +0200

    no cors

--- kantineApp/settings.py
+++ kantineApp/settings.py
@@ -45,11 +45,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders'
+    # 'corsheaders'
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -103,7 +103,7 @@ DATABASES = {
     }
 }
 
-
+print(environ.items())
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
