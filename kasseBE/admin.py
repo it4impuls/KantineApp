7a209322aa5b9d152e9748a095e9072e1a3c1f0d
marilynias <marilynia57+github@gmail.com>
2025-08-08 10:46:14 +0200

    add total/total with tax to admin

--- kasseBE/admin.py
+++ kasseBE/admin.py
@@ -30,5 +30,12 @@ class OrderInline(admin.TabularInline):
 
 @admin.register(OrderBill)
 class OrderBillAdmin(admin.ModelAdmin):
-    search_fields = list_display =list_display_links = ["id", "month" ]
+    @admin.display(description="total")
+    def total(self, obj):
+        return sum(order.ordered_item for order in obj.order_set.all())
+    @admin.display(description="total with tax")
+    def total_with_tax(self, obj):
+        return sum(order.ordered_item+(order.ordered_item*order.tax/100) for order in obj.order_set.all())
+    search_fields = list_display =list_display_links = ["id", "month" , 'total', 'total_with_tax']
+    readonly_fields = ['total', 'total_with_tax']
     inlines = [OrderInline]
