abe16a5d89be147296196b615a83db34ef7dd1d8
marilynias <marilynia57+github@gmail.com>
2025-08-26 12:19:31 +0200

    split workflows

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 set -x      # echo output
 dirname=${PWD##*/}
-target_dir=$HOME/$dirname
+target_dir=/tmp/$dirname
 
-# rm -r "$target_dir"
 mkdir "$target_dir"
 cp -rf "$PWD/"* "$target_dir"
-cp -f $HOME/.env $target_dir
+cp -f $HOME/.env $target_dir/
 cd "$target_dir"
-# ls -a
 
 # Docker
 docker compose up -d --build --remove-orphans
