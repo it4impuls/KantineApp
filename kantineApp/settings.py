f93f632921353f4d3c5ca204256aa21b1844d85e
marilynias <marilynia57+github@gmail.com>
2025-08-28 09:34:27 +0200

    debug

--- kantineApp/settings.py
+++ kantineApp/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-9@e#u@ku@*$_y@1o80$a0wjohz49t@!q&wotmcfsnw(l+v$!8*
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = environ.get("DEBUG", False)
-# DEBUG = False
+# DEBUG = environ.get("DEBUG", False)
+DEBUG = True
 # ALLOWED_HOSTS = []
 if DEBUG:
     ALLOWED_HOSTS = ["*"]
