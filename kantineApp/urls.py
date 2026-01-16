eaeb45963d78b16257dc96f37b430046e7340603
Marilynias <marilynia57@gmail.com>
2026-01-16 13:36:12 +0100

    perma redirect

--- kantineApp/urls.py
+++ kantineApp/urls.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path, include
 from rest_framework import routers, serializers, viewsets
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .views import is_loggedin, login, get_csrf_token
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', include('kasseBE.urls')),
-    path('admin/', admin.site.urls),
-    path('api/', include('rest_framework.urls', namespace='rest_framework')),
-    path('auth/login/', login, name='login'),
-    path('auth/verify/', is_loggedin, name='logged_in'),
-    path('csrf/', get_csrf_token, name='get_csrf_token'),
+    re_path(r'^', RedirectView.as_view(url='https://kantine.impuls-reha.de')),
+    # path('admin/', admin.site.urls),
+    # path('api/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('auth/login/', login, name='login'),
+    # path('auth/verify/', is_loggedin, name='logged_in'),
+    # path('csrf/', get_csrf_token, name='get_csrf_token'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
